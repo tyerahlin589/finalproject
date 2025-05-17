c3a33e4d46ba4130bd6721bb83ff6be0360ebaf2
tyerahlin589 <tyerahlin589@gmail.com>
2025-05-17 05:51:04 +0000

    Add 33.py

---
+++ src/33.py
@@ -0,0 +1,19 @@
+def add_numbers(x, y):
+    """ Add two numbers """
+    result = x + y
+    return result
+
+def subtract_numbers(x, y):
+    """ Subtract the second number from the first one """
+    difference = x - y
+    return difference
+
+def multiply_numbers(x, y):
+    """ Multiply two numbers """
+    product = x * y
+    return product
+
+def divide_numbers(x, y):
+    """ Divide one number by another """
+    quotient = x / y
+    return quotient
