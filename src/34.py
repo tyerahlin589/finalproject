bf6b91017cf925b8deb22c5a47b4877c23528fc5
tyerahlin589 <tyerahlin589@gmail.com>
2025-05-18 22:38:25 +0000

    Add 34.py

---
+++ src/34.py
@@ -0,0 +1,14 @@
+def square_root(n):
+    """
+    This function calculates and returns the square root of a given number n.
+    
+    Example usage:
+    >>> square_root(9)
+    3.0
+    >>> square_root(25)
+    5.0
+    """
+    return n ** 0.5
+
+# Test the function with some data points
+print(square_root(4))  # Expected output: 2.0
